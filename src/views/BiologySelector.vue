28df8b2f5698ca58d9af6b142bf879c76f1ee80d
HakimYu <yhjredin@gmail.com>
2023-04-30 13:23:32 +0800

    UI update:BiologySelector

--- src/views/BiologySelector.vue
+++ src/views/BiologySelector.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup xmlns="http://www.w3.org/1999/html">
 import {ref} from 'vue';
-
+import { useDisplay } from 'vuetify'
 const content: Array<{ name: string, description: string, id: number }> = [
   {
     name: "必修一",
@@ -33,37 +33,53 @@ const content: Array<{ name: string, description: string, id: number }> = [
     id: 5,
   },
 ];
-
-
+const { mdAndUp } = useDisplay();
+function getSetence(){
+  fetch('https://v1.hitokoto.cn')
+      .then(response => response.json())
+      .then(data => {
+        return data.hitokoto;
+      })
+      .catch(console.error)
+}
 let items = ref(content);
-let select = ref({name: ""});
+let selectedBooks = ref([{name: ""}]);
+let selectedQuestions = ref([]);
 </script>
 
 <template>
-  <v-container class="d-flex">
-    <v-row class="d-flex justify-center align-self-end">
-      <v-col cols="4">
-        <v-select
-            v-model="select"
-            :items="items"
-            item-title="name"
-            label="Book"
-            persistent-hint
-            return-object
-        >
-        </v-select>
-      </v-col>
-      <v-col cols="6">
-        <v-select
-            v-model="select"
-            :items="items"
-            item-title="name"
-            label="Chapter"
-            persistent-hint
-            return-object
-        >
-        </v-select>
-      </v-col>
-    </v-row>
+  <v-container>
+    <p>{{ selectedQuestions }}</p>
+    <div class="overflow-auto overflow-x-hidden"
+         :style="{ height: mdAndUp ? '80vh' : '70vh' }">
+      <v-row class="justify-center">
+        <v-col v-for="n in 200" class="d-flex" lg="6" sm="5">
+          <v-card class="justify-center" width="70vh">
+            <v-card-item density="compact">
+            <v-checkbox :label="n"
+                        :value="n"
+                        v-model="selectedQuestions">
+            </v-checkbox>
+            <v-card-text class="text-h6 justify-center">{{ "a".repeat(n) }}</v-card-text>
+            </v-card-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+    <v-container class="d-flex" style="position: absolute;bottom: 0">
+      <v-row class="justify-center">
+        <v-col cols="6">
+          <v-select
+              v-model="selectedBooks"
+              :items="items"
+              item-title="name"
+              label="Book"
+              persistent-hint
+              return-object
+          >
+          </v-select>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
