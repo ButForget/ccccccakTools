3a79a86cc276bd2c90532bd9d6ada5e1d2620a7a
CaK <butforget@outlook.com>
2023-07-01 21:24:20 +0800

    refactor.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { router } from "./route";
 import { createVuetify } from "vuetify/lib/framework.mjs";
 import { createPinia } from "pinia";
 import { createPersistedState } from "pinia-plugin-persistedstate";
-
+import {md3} from "vuetify/blueprints"
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 const app = createApp(App);
@@ -13,7 +13,7 @@ const pinia = createPinia();
 pinia.use(createPersistedState());
 
 app.use(router);
-app.use(createVuetify());
+app.use(createVuetify({blueprint: md3}));
 app.use(pinia);
 router.push({name: "selector"});
 app.mount("#app");
--- src/views/BiologySelector.vue
+++ src/views/BiologySelector.vue
@@ -1,56 +1,40 @@
 <script lang="ts" setup>
-import {reactive, watch} from 'vue';
-import {useDisplay} from 'vuetify'
-import {useRouter} from 'vue-router';
-import {bioStore} from '../store';
-import {resolveResource} from "@tauri-apps/api/path"
-import {readTextFile} from "@tauri-apps/api/fs"
-import { onBeforeMount } from 'vue';
+import { Ref, ref, reactive, watch } from 'vue';
+import { useDisplay } from 'vuetify'
+import { useRouter } from 'vue-router';
+import { bioStore } from '../store';
+import { resolveResource } from "@tauri-apps/api/path"
+import { readTextFile } from "@tauri-apps/api/fs"
+interface Book {
+  name: string,
+  description: string,
+  uri: string
+}
+let Books: Ref<Book[]> = ref([]);
+resolveResource("./resource/books_map.json")
+  .then((path) => {
+    readTextFile(path).then((value) => {
+      Books.value = JSON.parse(value)['map'];
+    })
+  })
 
-const Books: Array<{ name: string, description: string, uri: string }> = [
-  {
-    name: "必修一",
-    description: "分子与细胞",
-    uri: "BX_1.json"
-  },
-  {
-    name: "必修二",
-    description: "遗传与进化",
-    uri: "BX_2.json"
-  },
-  {
-    name: "选择性必修一",
-    description: "稳态与调节",
-    uri: "XB_1.json"
-  },
-  {
-    name: "选择性必修二",
-    description: "生物与环境",
-    uri: "XB_2.json"
-  },
-  {
-    name: "选择性必修三",
-    description: "生物技术与工程",
-    uri: "XB_3.json"
-  },
-];
 const { mdAndUp } = useDisplay();
 const router = useRouter();
 const store = bioStore();
 //selector 内部状态
 interface selectorState {
-  selectedBooks: {name: string, description: string, uri: string} 
+  selectedBooks: Book
   selectedQuestions: string[][]
   questions: string[][]
   menu: boolean | false
   randomValue: number | 0
 }
 const state: selectorState = reactive({
-  selectedBooks: {name: "", description: "", uri: ""},
+  selectedBooks: { name: "", description: "", uri: "" },
   selectedQuestions: [],
   questions: [],
   menu: false,
-  randomValue:0
+  randomValue: 0
 })
 
 
@@ -58,9 +42,9 @@ const state: selectorState = reactive({
 //检测用户是否选中书目, 若选中, 获取该书目对应的资源
 watch(() => state.selectedBooks, () => {
   let book = state.selectedBooks;
-  if(book == undefined || book.name == "") return;
+  if (book == undefined || book.name == "") return;
 
-  resolveResource("./resource" + book.uri)
+  resolveResource("./resource/bioQuestions/" + book.uri)
     .then((path) => {
       readTextFile(path).then((value) => {
         state.questions = JSON.parse(value)['list'];
@@ -68,25 +52,25 @@ watch(() => state.selectedBooks, () => {
     })
 })
 
-function toPlay(): void{
+function toPlay(): void {
   store.questions = [];
   store.questions = state.selectedQuestions;
-  router.push({name: "player"});
+  router.push({ name: "player" });
 }
 
-function selectAll(){
+function selectAll() {
   state.selectedQuestions = state.questions;
 }
 
-function removeAll(){
+function removeAll() {
   state.selectedQuestions = [];
 }
 
-function randomSelect(){
+function randomSelect() {
   let n = state.randomValue;
   let result = [];
   let copyArr = state.questions?.slice(); // 复制原数组，避免修改原数组
-  while (n > 0 && copyArr!==undefined) {
+  while (n > 0 && copyArr !== undefined) {
     const randomIndex = Math.floor(Math.random() * copyArr.length);
     result.push(copyArr[randomIndex]);
     copyArr?.splice(randomIndex, 1); // 删除已经取出的元素
@@ -101,23 +85,26 @@ function randomSelect(){
     <div class="overflow-auto overflow-x-hidden"
          :style="{ height: mdAndUp ? '80vh' : '70vh' }">
       <v-row class="justify-center">
+        <!-- 
+          Questions list.
+         -->
         <v-col 
           v-if="state.selectedBooks !== undefined && state.questions !== undefined"
           v-for="item in state.questions"
           class="d-flex justify-center" lg="4" md="6" sm="7" cols="10"
         >
           <v-lazy :min-height="200" :options="{'threshold':0.5}" transition="fade-transition">
-          <v-card class="justify-center w-100">
-            <v-card-item density="compact">
+            <v-card class="justify-center w-100">
+              <v-card-item density="compact">
             <v-checkbox
               :label="item[0]"
               :value="item"
               v-model="state.selectedQuestions"
             >
-            </v-checkbox>
-            <v-card-text class="text-h6 justify-center">{{ item[1] }}</v-card-text>
-            </v-card-item>
-          </v-card>
+                </v-checkbox>
+                <v-card-text class="text-h6 justify-center">{{ item[1] }}</v-card-text>
+              </v-card-item>
+            </v-card>
           </v-lazy>
         </v-col>
       </v-row>
@@ -126,18 +113,19 @@ function randomSelect(){
       <v-row class="justify-end">
         <v-col class="justify-center float-left d-inline-flex">
           <v-btn class="mx-2" :icon="state.selectedQuestions.length!==0?'mdi-select-remove':'mdi-select-all'" @click="state.selectedQuestions.length!==0?removeAll():selectAll()"></v-btn>
-          <div class="text-center">
+        <div class="text-center">
             <v-menu
                 v-model="state.menu"
                 :close-on-content-click="false"
                 location="top"
             >
-              <template v-slot:activator="{ props }">
-                <v-btn v-bind="props" class="mx-2" icon="mdi-dice-multiple-outline"></v-btn>
-              </template>
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" class="mx-2" icon="mdi-dice-multiple-outline"></v-btn>
+            </template>
 
-              <v-card min-width="300">
-                <v-card-actions>
+            <v-card min-width="300">
+              <v-card-actions>
+                <v-expand-transition>
                   <v-slider
                       class="mt-6"
                       v-model="state.randomValue"
@@ -146,17 +134,18 @@ function randomSelect(){
                       :step="1"
                       thumb-label
                   ></v-slider>
-                  <v-btn icon="mdi-close" @click="state.menu = false"></v-btn>
-                  <v-btn icon="mdi-check" @click="randomSelect()"></v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-menu>
-          </div>
+                </v-expand-transition>
+                <v-btn icon="mdi-close" @click="state.menu = false"></v-btn>
+                <v-btn icon="mdi-check" @click="randomSelect()"></v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+        </div>
         </v-col>
         <v-col cols="6">
           <v-select
             v-model="state.selectedBooks"
-            :items="Books"
+            :items="Books.length == 0? []: Books"
             :hint="`${state.selectedBooks.name === ''? 'wait for a select': state.selectedBooks.description}`"
             item-title="name"
             label="Book"
@@ -166,7 +155,7 @@ function randomSelect(){
           </v-select>
         </v-col>
         <v-col cols="3">
-          <v-btn icon="mdi-check" @click="toPlay"></v-btn>
+        <v-btn icon="mdi-check" @click="toPlay"></v-btn>
         </v-col>
       </v-row>
     </v-container>
